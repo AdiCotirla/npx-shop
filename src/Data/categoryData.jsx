b1f7f34b4ce37466239a0fe1858708595efa3304
AdiCotirla <adioprea53@gmail.com>
2022-01-12 01:36:54 +0200

    dep

--- src/Data/categoryData.jsx
+++ src/Data/categoryData.jsx
@@ -3,24 +3,24 @@ export const Categories =[
     nume: "SUPREME",
     image: 'https://cdn.shopify.com/s/files/1/0520/8974/9672/products/2021-04-01_22.10.08_720x.png?v=1617304289',
     clasa : "supreme",
-    pozitie: "/supreme"
+    pozitie: "/npx-shop/supreme"
     },
     {
         nume: "OFF-WHITE",
         image: 'https://cdn.shopify.com/s/files/1/0520/8974/9672/products/2020-12-18_17.34.01_720x.png?v=1608305691',
         clasa : "off",
-        pozitie: "/offwhite"
+        pozitie: "/npx-shop/offwhite"
     },
         {
             nume : "BAPE",
             image: 'https://cdn.shopify.com/s/files/1/0520/8974/9672/products/2020-12-18_16.18.22_540x.png?v=1608301369',
             clasa : "bape",
-            pozitie: "/bape"
+            pozitie: "/npx-shop/bape"
         },
         {
             nume : 'PALM-ANGELS',
         image: 'https://cdn.shopify.com/s/files/1/0520/8974/9672/products/2020-12-18_16.58.05_540x.png?v=1608304294',
         clasa: 'angels',
-        pozitie: '/palmangels'
+        pozitie: '/npx-shop/palmangels'
     }
 ]
